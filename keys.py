bf9893c08a16bc12b4b8e0b53d4638e4d35df571
Megha Agarwal <agarwal22megha@gmail.com>
2019-06-25 19:51:37 +0000

    Parallelize processing, Output analysis

---
+++ keys.py
@@ -0,0 +1,4 @@
+bing_spell_check_key = "aa823ca687fb41b7a81b2b2631b886b5"
+spell_check_url = "https://api.cognitive.microsoft.com/bing/v7.0/spellcheck"
+subscription_key = "3ae4612060634dbd9e339d459059e709"
+text_analytics_base_url = "https://uksouth.api.cognitive.microsoft.com/text/analytics/v2.0/"
